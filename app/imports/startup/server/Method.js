7a2966d236a8588b29df62a3b747abdc53cb483b
maia <31334240+maiabe@users.noreply.github.com>
2024-03-19 02:18:21 -1000

    rebase branch

--- app/imports/startup/server/Method.js
+++ app/imports/startup/server/Method.js
@@ -3,9 +3,9 @@ import { HTTP } from 'meteor/http';
 import cloudinary from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: 'djoahlpoc',
-  api_key: '513547556632438',
-  api_secret: 'Fp6cEWLBdbmAfrQlshHfA-jOgXs',
+  cloud_name: 'YOUR-KEY-HERE',
+  api_key: 'YOUR-KEY-HERE',
+  api_secret: 'YOUR-KEY-HERE',
 });
 
 Meteor.methods({
@@ -15,7 +15,7 @@ Meteor.methods({
       const response = await HTTP.call('POST', 'https://api.openai.com/v1/images/generations', {
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${'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'}`,
+          Authorization: `Bearer ${'YOUR-KEY-HERE'}`,
         },
         data: {
           model: 'dall-e-3',
@@ -52,7 +52,7 @@ Meteor.methods({
       const response = await HTTP.call('POST', 'https://api.openai.com/v1/moderations', {
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${'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'}`,
+          Authorization: `Bearer ${'YOUR-KEY-HERE'}`,
         },
         data: {
           input: text,
