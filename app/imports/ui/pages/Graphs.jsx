7b8cf18d761969e84f383e22fdfa16988f153644
Tiffany Ngo <ngotiff15@gmail.com>
2024-04-10 19:07:52 -1000

    setup blank page for graphs + edited ReadMe file

---
+++ app/imports/ui/pages/Graphs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Graphs = () => (
+  <div>
+    <h2>Weekly Fitness Graph</h2>
+    <canvas id="fitnessChart" width="400" height="200" />
+  </div>
+);
+
+export default Graphs;
